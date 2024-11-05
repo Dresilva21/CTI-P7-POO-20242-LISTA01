16787b42ec4dae5e43c891d83250a43ceb72b434
André Gonçalves <andre.goncalves08@aluno.ifce.edu.br>
2024-11-05 13:46:41 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE03/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double n1 = 8.5 , n2 = 10, n3 = 5, peso1 = 1 , peso2 = 2 , peso3  = 3, media;
+		media = (n1*peso1 + n2*peso2 + n3*peso3) / (peso1+peso2+peso3);
+		System.out.println("A média deu :" + media);
+
+	}
+
+}
